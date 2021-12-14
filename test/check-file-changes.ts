23706ef0d666a68dde31376f431714435fecd78d
estherkim <esth@google.com>
2021-12-14 14:51:01 -0500

    all configs

--- test/check-file-changes.ts
+++ test/check-file-changes.ts
@@ -2,16 +2,15 @@ import assert from 'assert';
 import minimist from 'minimist';
 
 const {diff} = minimist(process.argv.slice(2));
-const files = diff.split(' ');
+const files: string[] = diff ? diff.split(' ') : [];
 
 describe('check file changes', () => {
   it('should not update more than one config at a time', () => {
-    let changes = 0;
-    for (const file of files) {
-      if (file.endsWith('versions.json') || file.endsWith('client-side-experiments.json')) {
-        changes ++;
-      }
-    }
-    assert.ok(changes <= 1, `Found ${changes} updates`);
+    const changes = files.filter(file => 
+      file.startsWith('configs/') && file.endsWith('.json')
+    );
+    assert.ok(
+      changes.length <= 1,
+      `Only 1 config change allowed at a time. Found ${changes.length}: ${changes.join(', ')}`)
   })
 })
