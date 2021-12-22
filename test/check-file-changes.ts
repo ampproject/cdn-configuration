7b2e9ec0bede84c29b16da15e1a2d4a44f8f9a90
Esther Kim <44627152+estherkim@users.noreply.github.com>
2021-12-22 14:10:41 -0500

    Add freeze test (#20)

--- test/check-file-changes.ts
+++ test/check-file-changes.ts
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import minimist, { ParsedArgs } from 'minimist';
+import freezedatesJson from '../configs/freezedates.json';
 
 interface Args extends ParsedArgs {
   diff?: string
@@ -16,4 +17,29 @@ describe('check file changes', () => {
       changes.length <= 1,
       `Only 1 config change allowed at a time. Found ${changes.length}: ${changes.join(', ')}`)
   })
+
+  it('should not change `versions.json` during a freeze', () => {
+    const change = files.some(file => 
+      file.includes('configs/versions.json')
+    );
+    
+    if (!change) {
+      return;
+    }
+
+    let currentFreeze = null;
+    const today = new Date(Date.now());
+
+    for (const freezedate of freezedatesJson.freezedates) {
+      const start = new Date(freezedate.start);
+      const end = new Date(freezedate.end);
+
+      if (start <= today && today <= end) {
+        currentFreeze = freezedate;
+        break;
+      }
+    }
+
+    assert.ok(!currentFreeze, '`versions.json` cannot be changed during a release freeze: ' + JSON.stringify(currentFreeze))
+  });
 })
