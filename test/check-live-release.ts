220eb7df2281a5f89afd2d18e1807f1888a022c3
estherkim <esth@google.com>
2021-12-28 15:12:18 -0500

    pretty

--- test/check-live-release.ts
+++ test/check-live-release.ts
@@ -10,7 +10,7 @@ const {diff}: Args = minimist(process.argv.slice(2));
 const files: string[] = diff ? diff.split(' ') : [];
 
 // TODO(estherkim): unskip when versions.json is used in prod
-describe.skip('check releases are live before promoting', function() {
+describe.skip('check releases are live before promoting', function () {
   const versionChannels = new Map<string, string[]>();
   for (const [channel, version] of Object.entries(versionsJson)) {
     if (version) {
@@ -20,18 +20,18 @@ describe.skip('check releases are live before promoting', function() {
     }
   }
 
-  before(function() {
+  before(function () {
     const change = files.some((file) => file.includes('configs/versions.json'));
     if (!change) {
       this.skip();
     }
-  })
+  });
 
   versionChannels.forEach((channels, version) => {
     it(`${version} (${channels.sort().join(', ')})`, async () => {
-        const url = `https://cdn.ampproject.org/rtv/${version}/v0.js`;
-        const response = await fetch(url, {method: 'GET'});
-        assert.equal(response.status, 200, response.statusText);
-      });
+      const url = `https://cdn.ampproject.org/rtv/${version}/v0.js`;
+      const response = await fetch(url, {method: 'GET'});
+      assert.equal(response.status, 200, response.statusText);
     });
+  });
 });
