32e9793524c9f6c280bf95108780d541e5ddc474
estherkim <esth@google.com>
2022-02-14 13:29:42 -0500

    nits

--- test/scripts/get-missing-cherry-picks.test.ts
+++ test/scripts/get-missing-cherry-picks.test.ts
@@ -4,7 +4,7 @@ import {GetResponseTypeFromEndpointMethod} from '@octokit/types';
 import {spy, when, deepEqual, resetCalls, reset} from 'ts-mockito';
 import {getMissingCommits} from '../../scripts/get-missing-cherry-picks-utils';
 
-describe('cases where cherry-picks are missing', () => {
+describe('get missing cherry-picks tests', () => {
   const octokit = new Octokit();
   const spiedRepos = spy(octokit.rest.repos);
   type CompareResponse = GetResponseTypeFromEndpointMethod<
@@ -19,213 +19,217 @@ describe('cases where cherry-picks are missing', () => {
     reset(spiedRepos);
   });
 
-  it('stable has 2 cherry-picks that are not in beta-opt-in', async () => {
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202070001000...2202070001002',
-        })
-      )
-    ).thenResolve({
-      status: 200,
-      data: {
-        commits: [
-          {
-            commit: {
-              message: `\
+  describe('cases where cherry-picks are missing', () => {
+    it('stable has 2 cherry-picks that are not in beta-opt-in', async () => {
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead: '2202070001000...2202070001002',
+          })
+        )
+      ).thenResolve({
+        status: 200,
+        data: {
+          commits: [
+            {
+              commit: {
+                message: `\
               First PR to fix bug (#29041)
               
               * update worker-dom
               * yarn lock
               
               (cherry picked from commit 725595ac41082b0f9db3103143b04070b0fa7a9e)`,
+              },
             },
-          },
-          {
-            commit: {
-              message: `\
+            {
+              commit: {
+                message: `\
               Second PR to fix bug (#29051)
               
               (cherry picked from commit b0fd442d5abc691a3d6eea076eccb83525258b7b)`,
+              },
             },
-          },
-        ],
-      },
-    } as CompareResponse);
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202090001000...725595ac41082b0f9db3103143b04070b0fa7a9e',
-        })
-      )
-    ).thenResolve({
-      status: 200,
-      data: {status: 'diverged'},
-    } as CompareResponse);
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202090001000...b0fd442d5abc691a3d6eea076eccb83525258b7b',
-        })
-      )
-    ).thenResolve({
-      status: 200,
-      data: {status: 'ahead'},
-    } as CompareResponse);
+          ],
+        },
+      } as CompareResponse);
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead:
+              '2202090001000...725595ac41082b0f9db3103143b04070b0fa7a9e',
+          })
+        )
+      ).thenResolve({
+        status: 200,
+        data: {status: 'diverged'},
+      } as CompareResponse);
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead:
+              '2202090001000...b0fd442d5abc691a3d6eea076eccb83525258b7b',
+          })
+        )
+      ).thenResolve({
+        status: 200,
+        data: {status: 'ahead'},
+      } as CompareResponse);
 
-    const releases = new Set([
-      '2202060001000', // lts
-      '2202070001002', // stable
-      '2202080001000', // beta-traffic
-    ]);
-    const commits = await getMissingCommits(octokit, '2202090001000', releases);
-    assert(commits.length == 2);
-    assert(
-      [
-        '725595ac41082b0f9db3103143b04070b0fa7a9e',
-        'b0fd442d5abc691a3d6eea076eccb83525258b7b',
-      ].every((c) => commits.includes(c))
-    );
-  });
+      const releases = new Set([
+        '2202060001000', // lts
+        '2202070001002', // stable
+        '2202080001000', // beta-traffic
+      ]);
+      const commits = await getMissingCommits(
+        octokit,
+        '2202090001000',
+        releases
+      );
+      assert(commits.length == 2);
+      assert(commits.includes('725595ac41082b0f9db3103143b04070b0fa7a9e'));
+      assert(commits.includes('b0fd442d5abc691a3d6eea076eccb83525258b7b'));
+    });
 
-  it('stable and lts have the same cherry-pick that is not in beta-opt-in', async () => {
-    const compareReleasesResponse = {
-      status: 200,
-      data: {
-        commits: [
-          {
-            commit: {
-              message: `\
+    it('stable and lts have the same cherry-pick that is not in beta-opt-in', async () => {
+      const compareReleasesResponse = {
+        status: 200,
+        data: {
+          commits: [
+            {
+              commit: {
+                message: `\
               First PR to fix bug (#29041)
               
               * update worker-dom
               * yarn lock
               
               (cherry picked from commit 725595ac41082b0f9db3103143b04070b0fa7a9e)`,
+              },
             },
-          },
-        ],
-      },
-    } as CompareResponse;
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202060001000...2202060001001',
-        })
-      )
-    ).thenResolve(compareReleasesResponse);
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202070001000...2202070001001',
-        })
-      )
-    ).thenResolve(compareReleasesResponse);
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202090001000...725595ac41082b0f9db3103143b04070b0fa7a9e',
-        })
-      )
-    ).thenResolve({
-      status: 200,
-      data: {status: 'diverged'},
-    } as CompareResponse);
+          ],
+        },
+      } as CompareResponse;
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead: '2202060001000...2202060001001',
+          })
+        )
+      ).thenResolve(compareReleasesResponse);
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead: '2202070001000...2202070001001',
+          })
+        )
+      ).thenResolve(compareReleasesResponse);
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead:
+              '2202090001000...725595ac41082b0f9db3103143b04070b0fa7a9e',
+          })
+        )
+      ).thenResolve({
+        status: 200,
+        data: {status: 'diverged'},
+      } as CompareResponse);
 
-    const releases = new Set([
-      '2202060001001', // lts
-      '2202070001001', // stable
-      '2202080001000', // beta-traffic
-    ]);
-    const commits = await getMissingCommits(octokit, '2202090001000', releases);
-    assert(commits.length == 1);
-    assert(commits[0] == '725595ac41082b0f9db3103143b04070b0fa7a9e');
-  });
-});
-
-describe('cases where cherry-picks are not missing', () => {
-  const octokit = new Octokit();
-  const spiedRepos = spy(octokit.rest.repos);
-  type CompareResponse = GetResponseTypeFromEndpointMethod<
-    typeof octokit.rest.repos.compareCommitsWithBasehead
-  >;
-
-  afterEach(() => {
-    resetCalls(spiedRepos);
+      const releases = new Set([
+        '2202060001001', // lts
+        '2202070001001', // stable
+        '2202080001000', // beta-traffic
+      ]);
+      const commits = await getMissingCommits(
+        octokit,
+        '2202090001000',
+        releases
+      );
+      assert(commits.length == 1);
+      assert(commits.includes('725595ac41082b0f9db3103143b04070b0fa7a9e'));
+    });
   });
 
-  after(() => {
-    reset(spiedRepos);
-  });
-
-  it('beta-traffic has a cherry-pick that was included in beta-opt-in cut', async () => {
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202080001000...2202080001001',
-        })
-      )
-    ).thenResolve({
-      status: 200,
-      data: {
-        commits: [
-          {
-            commit: {
-              message: `\
+  describe('cases where cherry-picks are not missing', () => {
+    it('beta-traffic has a cherry-pick that was included in beta-opt-in cut', async () => {
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead: '2202080001000...2202080001001',
+          })
+        )
+      ).thenResolve({
+        status: 200,
+        data: {
+          commits: [
+            {
+              commit: {
+                message: `\
               First PR to fix bug (#29041)
               
               * update worker-dom
               * yarn lock
               
               (cherry picked from commit 725595ac41082b0f9db3103143b04070b0fa7a9e)`,
+              },
             },
-          },
-        ],
-      },
-    } as CompareResponse);
-    when(
-      spiedRepos.compareCommitsWithBasehead(
-        deepEqual({
-          owner: 'ampproject',
-          repo: 'amphtml',
-          basehead: '2202090001000...725595ac41082b0f9db3103143b04070b0fa7a9e',
-        })
-      )
-    ).thenResolve({
-      status: 200,
-      data: {status: 'behind'},
-    } as CompareResponse);
+          ],
+        },
+      } as CompareResponse);
+      when(
+        spiedRepos.compareCommitsWithBasehead(
+          deepEqual({
+            owner: 'ampproject',
+            repo: 'amphtml',
+            basehead:
+              '2202090001000...725595ac41082b0f9db3103143b04070b0fa7a9e',
+          })
+        )
+      ).thenResolve({
+        status: 200,
+        data: {status: 'behind'},
+      } as CompareResponse);
 
-    const releases = new Set([
-      '2202060001000', // lts
-      '2202070001000', // stable
-      '2202080001001', // beta-traffic
-    ]);
-    const commits = await getMissingCommits(octokit, '2202090001000', releases);
-    assert(commits.length == 0);
-  });
+      const releases = new Set([
+        '2202060001000', // lts
+        '2202070001000', // stable
+        '2202080001001', // beta-traffic
+      ]);
+      const commits = await getMissingCommits(
+        octokit,
+        '2202090001000',
+        releases
+      );
+      assert(commits.length == 0);
+    });
 
-  it('none of the channels have cherry-picks', async () => {
-    const releases = new Set([
-      '2202060001000', // lts
-      '2202070001000', // stable
-      '2202080001000', // beta-traffic
-    ]);
-    const commits = await getMissingCommits(octokit, '2202090001000', releases);
-    assert(commits.length == 0);
+    it('none of the channels have cherry-picks', async () => {
+      const releases = new Set([
+        '2202060001000', // lts
+        '2202070001000', // stable
+        '2202080001000', // beta-traffic
+      ]);
+      const commits = await getMissingCommits(
+        octokit,
+        '2202090001000',
+        releases
+      );
+      assert(commits.length == 0);
+    });
   });
 });
