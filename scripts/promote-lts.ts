bee52e7569855ba3ff0a1e9b700e5a8909b780bd
Esther Kim <44627152+estherkim@users.noreply.github.com>
2022-03-07 13:51:31 -0500

    Move "skip LTS" logic to the workflow level (#218)

--- scripts/promote-lts.ts
+++ scripts/promote-lts.ts
@@ -11,16 +11,6 @@ const {amp_version: AMP_VERSION} = yargs(process.argv.slice(2))
   .parseSync();
 
 void runPromoteJob(jobName, () => {
-  const dayOfMonth = new Date().getUTCDate();
-  if (!AMP_VERSION && !(8 <= dayOfMonth && dayOfMonth <= 14)) {
-    // Skip this job if today is not the 2nd Monday of the month. The 2nd Monday
-    // always falls between the 8th to the 14th of the month (inclusive).
-    console.log(
-      'Automated LTS promote only occur on the 2nd Monday of each month. Skipping...'
-    );
-    return Promise.resolve('');
-  }
-
   return createVersionsUpdatePullRequest((currentVersions) => {
     const ampVersion = AMP_VERSION || currentVersions.stable.slice(2);
 
