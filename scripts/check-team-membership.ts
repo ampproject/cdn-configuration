35bc0697a05b09139fb6dcccc7b15fcf1542c767
estherkim <esth@google.com>
2022-03-09 18:47:20 -0500

    add workflow and eslint rule

---
+++ scripts/check-team-membership.ts
@@ -0,0 +1,26 @@
+import yargs from 'yargs/yargs';
+import {Octokit} from '@octokit/rest';
+
+const {org, team: team_slug, user} = yargs(process.argv.slice(2))
+  .options({
+    org: {type: 'string', demandOption: true},
+    team: {type: 'string', demandOption: true},
+    user: {type: 'string', demandOption: true},
+  })
+  .parseSync();
+
+const octokit = new Octokit({
+  auth: process.env.READ_ORG,
+});
+
+async function checkTeamMembership() {
+  const {data: members} = await octokit.rest.teams.listMembersInOrg({
+    org,
+    team_slug,
+  })
+
+  const isMember = members.some(member => member.login == user);
+  process.stdout.write(`${isMember}`);
+}
+
+void checkTeamMembership();
