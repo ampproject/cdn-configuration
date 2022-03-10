370431926edd0890e904f0fb86bd6094e5fcbe0d
estherkim <esth@google.com>
2022-03-09 20:24:34 -0500

    check all approvers not just the recent

---
+++ scripts/check-qa-approval.ts
@@ -0,0 +1,48 @@
+import yargs from 'yargs/yargs';
+import {Octokit} from '@octokit/rest';
+
+const {pull_number} = yargs(process.argv.slice(2))
+  .options({pull_number: {type: 'number', demandOption: true}})
+  .parseSync();
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+async function getApprovers(): Promise<string[]> {
+  const {data: reviews} = await octokit.rest.pulls.listReviews({
+    owner: 'ampproject',
+    repo: 'cdn-configuration',
+    pull_number,
+  });
+
+  const approvers: string[] = [];
+  reviews.forEach((review) => {
+    if (review.state == 'approved' && review.user) {
+      approvers.push(review.user.login);
+    }
+  });
+
+  return approvers;
+}
+
+async function checkQaTeamMembership(approvers: string[]): Promise<boolean> {
+  const {data: members} = await octokit.rest.teams.listMembersInOrg({
+    org: 'ampproject',
+    team_slug: 'amp-qa',
+  });
+
+  return members.some((member) => approvers.includes(member.login));
+}
+
+async function setOutput() {
+  const approvers = await getApprovers();
+  if (approvers.length < 1) {
+    return;
+  }
+
+  const isQa = await checkQaTeamMembership(approvers);
+  process.stdout.write(`${isQa}`);
+}
+
+void setOutput();
