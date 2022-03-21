7a05d9c1f8c10fd6acfae0ac78e3f369ea2827d0
estherkim <esth@google.com>
2022-03-21 15:30:30 -0400

    add script

--- scripts/post-promote-tasks-job.ts
+++ scripts/post-promote-tasks-job.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
 import yargs from 'yargs/yargs';
-import {getVersionDiff} from './post-promote-tasks-utils';
+import {getVersionDiff, getBaseAmpVersion} from './post-promote-tasks-utils';
 
 const {head, base} = yargs(process.argv.slice(2))
   .options({
@@ -9,29 +9,49 @@ const {head, base} = yargs(process.argv.slice(2))
   })
   .parseSync();
 
-interface TaskConfig {
-  [channel: string]: string;
-}
-
-//TODO(estherkim): add release tagger tasks
-const PUBLISH_NPM: TaskConfig = {
+const PUBLISH_NPM: {[channel: string]: string} = {
   nightly: 'nightly',
   stable: 'latest',
 };
 
-const RELEASE_CALENDAR: TaskConfig = {
+const RELEASE_CALENDAR: {[channel: string]: string} = {
   nightly: 'nightly',
   'beta-traffic': 'beta',
   stable: 'stable',
   lts: 'lts',
 };
 
+const RELEASE_TAGGER: {[channel: string]: Record<string, string>} = {
+  'beta-opt-in': {
+    headChannel: 'beta-opt-in',
+    baseChannel: 'stable',
+  },
+  'beta-traffic': {
+    headChannel: 'beta-percent',
+    baseChannel: 'stable',
+  },
+  stable: {
+    headChannel: 'stable',
+    baseChannel: 'stable',
+  },
+  lts: {
+    headChannel: 'lts',
+    baseChannel: 'lts',
+  },
+};
+
 async function setOutput() {
   const versionDiff = await getVersionDiff(head, base);
   const npm: {'amp-version': string; tag: string}[] = [];
   const calendar: {'amp-version': string; channel: string}[] = [];
+  const tagger: {
+    action: string;
+    head: string;
+    base: string;
+    channel: string;
+  }[] = [];
 
-  versionDiff.forEach(({channel, version}) => {
+  for await (const {channel, version} of versionDiff) {
     if (PUBLISH_NPM[channel]) {
       npm.push({'amp-version': version, tag: PUBLISH_NPM[channel]});
     }
@@ -42,10 +62,24 @@ async function setOutput() {
         channel: RELEASE_CALENDAR[channel],
       });
     }
-  });
+
+    if (RELEASE_TAGGER[channel]) {
+      const {headChannel, baseChannel} = RELEASE_TAGGER[channel];
+      const baseAmpVersion = await getBaseAmpVersion(version, baseChannel);
+      if (baseAmpVersion) {
+        tagger.push({
+          action: 'promote', // TODO(estherkim): support rollbacks
+          head: version,
+          base: baseAmpVersion,
+          channel: headChannel,
+        });
+      }
+    }
+  }
 
   core.setOutput('npm', npm.length > 0 ? {includes: npm} : null);
   core.setOutput('calendar', calendar.length > 0 ? {includes: calendar} : null);
+  core.setOutput('tagger', tagger.length > 0 ? {includes: tagger} : null);
 }
 
 void setOutput();
