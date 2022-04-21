032199dc5fa947bdce106b646ade8fde3a72e37f
Esther Kim <44627152+estherkim@users.noreply.github.com>
2022-04-21 16:03:58 -0400

    Update error message for live release tests (#308)
    * error message
    
    * prettier

--- test/check-live-release.ts
+++ test/check-live-release.ts
@@ -29,7 +29,11 @@ describe('check releases are live before promoting', function () {
     it(`${version} (${channels.sort().join(', ')})`, async () => {
       const url = `https://ampjs.org/rtv/${version}/v0.js`;
       const response = await fetch(url, {method: 'GET'});
-      assert.equal(response.status, 200, response.statusText);
+      assert.equal(
+        response.status,
+        200,
+        `Error fetching ${url}: ${response.statusText}`
+      );
     });
   });
 });
