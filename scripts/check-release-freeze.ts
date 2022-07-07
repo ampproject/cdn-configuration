562067dd7a8dc2450ee008fee72ddf5e56921954
Esther Kim <44627152+estherkim@users.noreply.github.com>
2022-07-07 16:42:53 -0400

    Fix release freeze logic (#432)
    * add freeze check
    
    * prettier

---
+++ scripts/check-release-freeze.ts
@@ -0,0 +1,26 @@
+import * as core from '@actions/core';
+import freezedatesJson from '../configs/freezedates.json';
+
+function main() {
+  let freeze = false;
+  const today = new Date(Date.now());
+
+  for (const freezedate of freezedatesJson.freezedates) {
+    const start = new Date(freezedate.start);
+    const end = new Date(freezedate.end);
+
+    if (start <= today && today <= end) {
+      core.info(`There is a release freeze in effect today.`);
+      core.info(
+        `Date range: ${start.toLocaleDateString()} to ${end.toLocaleDateString()}`
+      );
+      core.info(`Description: ${freezedate.description}`);
+      freeze = true;
+      break;
+    }
+  }
+
+  core.setOutput('freeze', freeze);
+}
+
+main();
